bbfbebaca4e8e8d0190348a1e5a06fe162e4fcd5
Khizar-Abbas-Developer <khizarnot7@gmail.com>
2025-05-01 11:16:53 +0500

    rfd

--- app/product/wishlist/page.jsx
+++ app/product/wishlist/page.jsx
@@ -43,67 +43,67 @@ const WhishList = () => {
             <hr className="mb-4 hidden sm:block" />
             {data.map((item) => {
               return (
-                <div
-                  className="grid grid-cols-1 sm:grid-cols-5 sm:items-center w-full gap-y-6 sm:gap-y-0 my-3"
-                  key={item._id}
-                >
-                  {/* Product Info */}
-                  <div className="col-span-2 flex items-center gap-4">
-                    <div
-                      className="cursor-pointer text-gray-700 hover:text-red-600 border border-black p-1 rounded-full"
-                      onClick={handleRemoveProduct(item._id)}
-                    >
-                      <ImCross size={9} />
+                <React.Fragment key={item._id}>
+                  <div className="grid grid-cols-1 sm:grid-cols-5 sm:items-center w-full gap-y-6 sm:gap-y-0 my-3">
+                    {/* Product Info */}
+                    <div className="col-span-2 flex items-center gap-4">
+                      <div
+                        className="cursor-pointer text-gray-700 hover:text-red-600 border border-black p-1 rounded-full"
+                        onClick={handleRemoveProduct(item._id)}
+                      >
+                        <ImCross size={9} />
+                      </div>
+                      <Image
+                        src={item?.image || null}
+                        alt="product"
+                        width={60}
+                        height={60}
+                        className="object-cover rounded"
+                      />
+                      <p className="text-sm font-medium text-gray-800 hidden sm:block">
+                        {item?.productName}
+                      </p>
                     </div>
-                    <Image
-                      src={item?.image || null}
-                      alt="product"
-                      width={60}
-                      height={60}
-                      className="object-cover rounded"
-                    />
-                    <p className="text-sm font-medium text-gray-800 hidden sm:block">
-                      {item?.productName}
-                    </p>
-                  </div>
 
-                  {/* Product name for mobile */}
-                  <div className="sm:hidden">
-                    <p className="text-sm font-medium text-gray-800">
-                      {item?.productName}
-                    </p>
-                  </div>
+                    {/* Product name for mobile */}
+                    <div className="sm:hidden">
+                      <p className="text-sm font-medium text-gray-800">
+                        {item?.productName}
+                      </p>
+                    </div>
 
-                  {/* Unit Price */}
-                  <div className="text-sm text-start">
-                    <span className="sm:hidden font-semibold">Price: </span>
-                    From:{" "}
-                    <span className="font-bold">₨{item?.productPrice}</span>
-                  </div>
+                    {/* Unit Price */}
+                    <div className="text-sm text-start">
+                      <span className="sm:hidden font-semibold">Price: </span>
+                      From:{" "}
+                      <span className="font-bold">₨{item?.productPrice}</span>
+                    </div>
 
-                  {/* Stock + Buttons */}
-                  <div className="flex justify-start items-center gap-2">
-                    <span className="px-3 py-2 text-sm font-medium bg-green-400 text-black rounded cursor-pointer">
-                      In Stock
-                    </span>
-                  </div>
+                    {/* Stock + Buttons */}
+                    <div className="flex justify-start items-center gap-2">
+                      <span className="px-3 py-2 text-sm font-medium bg-green-400 text-black rounded cursor-pointer">
+                        In Stock
+                      </span>
+                    </div>
 
-                  {/* Buttons */}
-                  <div className="flex w-full flex-col sm:flex-col md:flex-col lg:flex-col xl:flex-row items-start sm:items-center justify-start gap-2">
-                    <button
-                      className="bg-[#5FA800] text-white px-3 py-2 text-sm font-semibold rounded w-full sm:w-auto cursor-pointer"
-                      onClick={handleAddToCart(item._id)}
-                    >
-                      Add to Cart
-                    </button>
-                    <button
-                      className="bg-[#5FA800] text-white px-[27px] py-2 text-sm font-semibold rounded w-full sm:w-auto cursor-pointer"
-                      onClick={handleRemoveProduct(item._id)}
-                    >
-                      Remove
-                    </button>
+                    {/* Buttons */}
+                    <div className="flex w-full flex-col sm:flex-col md:flex-col lg:flex-col xl:flex-row items-start sm:items-center justify-start gap-2">
+                      <button
+                        className="bg-[#5FA800] text-white px-3 py-2  md:px-[7px] text-sm font-semibold rounded w-full sm:w-auto cursor-pointer"
+                        onClick={handleAddToCart(item._id)}
+                      >
+                        Add to Cart
+                      </button>
+                      <button
+                        className="bg-[#5FA800] text-white px-[27px] py-2 text-sm font-semibold rounded w-full sm:w-auto cursor-pointer"
+                        onClick={handleRemoveProduct(item._id)}
+                      >
+                        Remove
+                      </button>
+                    </div>
                   </div>
-                </div>
+                  <hr className="text-gray-400" />
+                </React.Fragment>
               );
             })}
           </div>
