fc410f296d9cc6e6eaaf44f4f355506bc336a133
Khizar-Abbas-Developer <khizarnot7@gmail.com>
2025-05-03 07:21:45 +0500

    obvious

--- components/ProductDescription.jsx
+++ components/ProductDescription.jsx
@@ -6,10 +6,10 @@ const Description = ({ data }) => {
           key={item._id}
           className="max-w-full mx-auto mt-6 bg-white border border-gray-200 rounded-xl shadow-md px-5 py-4"
         >
-          <h2 className="text-xl font-semibold text-gray-800 mb-2 text-center">
+          <h2 className="text-xl font-semibold text-gray-800 mb-2 text-start">
             {item.heading}
           </h2>
-          <p className="text-sm text-gray-600 text-center">{item.detail}</p>
+          <p className="text-sm text-gray-600 text-start">{item.detail}</p>
         </div>
       ))}
     </>
